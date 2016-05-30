6b5e0847788bd8eaab844bcb559850223256fed2
jean <jean.tuffier@viacesi.fr>
2016-05-30 23:47:12 +0200

    continue test for DbManager

--- memoryorm/src/androidTest/java/no/hyper/memoryorm/DbManagerTest.java
+++ memoryorm/src/androidTest/java/no/hyper/memoryorm/DbManagerTest.java
@@ -1,13 +1,17 @@
 package no.hyper.memoryorm;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
 import junit.framework.Assert;
 
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -20,26 +24,56 @@ import org.junit.runner.RunWith;
 public class DbManagerTest {
 
     private static final String DB_NAME = "DbTest";
-    private Context context;
-    private DbManager manager;
+    private static final String TABLE_NAME = "Test";
+
+    private static  Context context;
+    private static DbManager manager;
+
+    @BeforeClass
+    public static void classSetUp() {
+        context = InstrumentationRegistry.getContext();
+        manager = new DbManager(context, DB_NAME, null, 1);
+    }
+
+    @AfterClass
+    public static void classCleanUp() {
+        context.deleteDatabase(DB_NAME);
+    }
 
     @Before
-    public void setUp() throws Exception {
-        context = InstrumentationRegistry.getContext();
-        context.deleteDatabase(DB_NAME);
-        manager = new DbManager(context, DB_NAME, null, 1);
+    public void start() {
+        manager.openDb();
+    }
+
+    @After
+    public void finish() {
+        manager.closeDb();
     }
 
     @Test
     public void shouldOpenDb() throws Exception {
-        manager.openDb();
         Assert.assertEquals(true, manager.isDbOpen());
     }
 
     @Test
-    public void shouldCloseDb() throws Exception {
-        manager.close();
-        Assert.assertEquals(false, manager.isDbOpen());
+    public void shouldExecute() throws Exception {
+        manager.execute("create table " + TABLE_NAME + " (id integer primary key, name text)");
+    }
+
+    @Test
+    public void shouldQuery() throws Exception {
+        Cursor cursor = manager.rawQuery("SELECT name FROM sqlite_master WHERE type='table' AND name='"
+                + TABLE_NAME  +"'", null);
+        Assert.assertEquals(1, cursor.getCount());
+    }
+
+    @Test
+    public void shouldInsert() throws Exception {
+        ContentValues values = new ContentValues();
+        values.put("id", 1);
+        values.put("name", "John Doe");
+        long rowId = manager.insert(TABLE_NAME, values);
+        Assert.assertEquals(1, rowId);
     }
 
 }
